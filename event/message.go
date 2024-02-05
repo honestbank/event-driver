5afc05a92c2c89977afc6da00962663b454d2b6f
Hansheng-Li <lihanshengkevin@hotmail.com>
2024-02-04 23:21:39 -0500

    change message

--- event/message.go
+++ event/message.go
@@ -1,10 +1,39 @@
 package event
 
-type Message interface {
-	GetKey() string
-	GetSource() string
-	GetContent() string
-	SetKey(string)
-	SetSource(string)
-	SetContent(string)
+type Message struct {
+	key     string
+	source  string
+	content string
+}
+
+func NewMessage(key, source, content string) *Message {
+	return &Message{
+		key:     key,
+		source:  source,
+		content: content,
+	}
+}
+
+func (m *Message) GetKey() string {
+	return m.key
+}
+
+func (m *Message) GetSource() string {
+	return m.source
+}
+
+func (m *Message) GetContent() string {
+	return m.content
+}
+
+func (m *Message) SetKey(key string) {
+	m.key = key
+}
+
+func (m *Message) SetSource(source string) {
+	m.source = source
+}
+
+func (m *Message) SetContent(content string) {
+	m.content = content
 }
