d262950af318d1007b3699623d65f9f7331275ad
Kelu Diao <1632092+lukecold@users.noreply.github.com>
2024-02-05 23:19:24 +0700

    feat: Add syntactic sugar for convert.ToKNativeEventHandler (#19)
    * feat: Add some syntactic sugar for convert.ToKNativeEventHandler
    
    Addressing issue https://github.com/lukecold/event-driver/issues/14
    
    One can convert the pipeline to a KNative event handler following this example:
    ```golang
    handleKNativeEvent := convert.ToKNativeEventHandler(
    	convert.CloudEventToInput,
    	myPipeline,
    	convert.OutputToCloudResult)
    cloudEventClient.StartReceiver(ctx, handleKNativeEvent)
    ```
    
    Also resolved Snyk high vulnerability risk https://app.snyk.io/org/lukecold/project/4169ecc4-8fa4-4948-8536-20ac3c8d95aa

--- convert/knative_event_handler.go
+++ convert/knative_event_handler.go
@@ -6,30 +6,69 @@ import (
 	"net/http"
 	"strings"
 
-	cloudevents "github.com/cloudevents/sdk-go/v2"
+	cloudEvents "github.com/cloudevents/sdk-go/v2"
 
 	"github.com/lukecold/event-driver/event"
 	"github.com/lukecold/event-driver/pipeline"
 )
 
+// InputConverter converts the KNativeEventHandler input to pipeline.Pipeline input.
+type InputConverter func(context.Context, *cloudEvents.Event) (*event.Message, error)
+
+// OutputConverter converts the pipeline.Pipeline output to KNativeEventHandler output.
+type OutputConverter func(context.Context, error) (*cloudEvents.Event, cloudEvents.Result)
+
+// KNativeEventHandler represents the most complicated function to
+// [start an event receiver](https://github.com/cloudevents/sdk-go/blob/main/v2/client/client.go#L33-L50),
+// in order to provide compatibility for all use cases.
+type KNativeEventHandler func(context.Context, cloudEvents.Event) (*cloudEvents.Event, cloudEvents.Result)
+
+// ToKNativeEventHandler converts the pipeline to a KNative event handler,
+// with an InputConverter to convert the KNativeEventHandler input to pipeline.Pipeline input,
+// and an OutputConverter to convert the pipeline.Pipeline output to KNativeEventHandler output.
 func ToKNativeEventHandler(
-	convertInput func(cloudevents.Event) (*event.Message, error),
+	convertInput InputConverter,
 	pipeline pipeline.Pipeline,
-	convertOutput func(error) (*cloudevents.Event, cloudevents.Result)) func(
-	ctx context.Context, event cloudevents.Event) (*cloudevents.Event, cloudevents.Result) {
-	return func(ctx context.Context, event cloudevents.Event) (*cloudevents.Event, cloudevents.Result) {
-		input, err := convertInput(event)
+	convertOutput OutputConverter) KNativeEventHandler {
+	return func(ctx context.Context, cloudEvent cloudEvents.Event) (*cloudEvents.Event, cloudEvents.Result) {
+		input, err := convertInput(ctx, &cloudEvent)
 		if err != nil {
-			return nil, cloudevents.NewHTTPResult(http.StatusBadRequest, "%s", err)
+			return nil, cloudEvents.NewHTTPResult(http.StatusBadRequest, "%s", err)
 		}
 		output := pipeline.Run(ctx, input)
 
-		return convertOutput(output)
+		return convertOutput(ctx, output)
 	}
 }
 
-func GetKey(event cloudevents.Event) (*string, error) {
-	rawKey := event.Extensions()["key"]
+// CloudEventToInput is a built-in InputConverter that could serve the basic functionalities of
+// converting a cloud event to a pipeline input message.
+func CloudEventToInput(_ context.Context, cloudEvent *cloudEvents.Event) (*event.Message, error) {
+	key, err := GetKey(cloudEvent)
+	if err != nil {
+		return nil, err
+	}
+	source, err := GetTopic(cloudEvent)
+	if err != nil {
+		return nil, err
+	}
+
+	return event.NewMessage(*key, *source, string(cloudEvent.Data())), nil
+}
+
+// OutputToCloudResult is a built-in OutputConverter that could serve the basic functionalities of
+// converting an error (the pipeline output) to the cloud event response.
+func OutputToCloudResult(_ context.Context, err error) (*cloudEvents.Event, cloudEvents.Result) {
+	if err != nil {
+		return nil, cloudEvents.NewHTTPResult(http.StatusInternalServerError, "%s", err)
+	}
+
+	return nil, cloudEvents.NewHTTPResult(http.StatusOK, "OK")
+}
+
+// GetKey tries to find the event key of string format under key `key`, in the extensions map.
+func GetKey(cloudEvent *cloudEvents.Event) (*string, error) {
+	rawKey := cloudEvent.Extensions()["key"]
 	if rawKey == nil {
 		return nil, errors.New("cannot find key from event extension")
 	}
@@ -41,8 +80,12 @@ func GetKey(event cloudevents.Event) (*string, error) {
 	return &key, nil
 }
 
-func GetTopic(event cloudevents.Event) (*string, error) {
-	source := event.Source()
+// GetTopic tries to find the topic that's stored as event source.
+func GetTopic(cloudEvent *cloudEvents.Event) (*string, error) {
+	source := cloudEvent.Source()
+	if len(source) == 0 {
+		return nil, errors.New("event source is empty")
+	}
 	split := strings.Split(source, "#")
 	if len(split) == 0 {
 		return nil, errors.New("cannot find topic from event source")
---
+++ convert/knative_event_handler_test.go
@@ -0,0 +1,112 @@
+package convert_test
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"testing"
+
+	v2 "github.com/cloudevents/sdk-go/v2"
+	cloudEvents "github.com/cloudevents/sdk-go/v2/event"
+	cloudEventTypes "github.com/cloudevents/sdk-go/v2/types"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/lukecold/event-driver/convert"
+	"github.com/lukecold/event-driver/event"
+	"github.com/lukecold/event-driver/pipeline"
+)
+
+const (
+	key   = "test-key"
+	topic = "test-topic"
+)
+
+type eventContent struct {
+	StringVal string
+	IntVal    int
+	FloatPtr  *float64
+}
+
+func TestGetKey(t *testing.T) {
+	t.Run("get key of string type", func(t *testing.T) {
+		cloudEvent := makeEvent(key, topic, nil)
+		parsedKey, err := convert.GetKey(cloudEvent)
+		assert.NoError(t, err)
+		assert.Equal(t, key, *parsedKey)
+	})
+
+	t.Run("fail if key cannot be casted to string", func(t *testing.T) {
+		cloudEvent := makeEvent(1, topic, nil)
+		_, err := convert.GetKey(cloudEvent)
+		assert.Error(t, err)
+	})
+
+	t.Run("failed if key isn't present in extension map", func(t *testing.T) {
+		cloudEvent := &cloudEvents.Event{
+			Context: cloudEvents.EventContextV03{
+				Source:          cloudEventTypes.URIRef{URL: url.URL{Fragment: topic}},
+				DataContentType: cloudEvents.StringOfApplicationJSON(),
+				Extensions:      map[string]interface{}{},
+			}.AsV03(),
+			DataEncoded: nil,
+		}
+		_, err := convert.GetKey(cloudEvent)
+		assert.Error(t, err)
+	})
+}
+
+func TestGetTopic(t *testing.T) {
+	t.Run("get topic from source", func(t *testing.T) {
+		cloudEvent := makeEvent(key, topic, nil)
+		parsedTopic, err := convert.GetTopic(cloudEvent)
+		assert.NoError(t, err)
+		assert.Equal(t, topic, *parsedTopic)
+	})
+
+	t.Run("fail if source is empty", func(t *testing.T) {
+		cloudEvent := makeEvent(key, "", nil)
+		_, err := convert.GetTopic(cloudEvent)
+		assert.Error(t, err)
+	})
+}
+
+func TestCloudEventToInput(t *testing.T) {
+	floatVal := 1.1
+	content, err := json.Marshal(eventContent{
+		StringVal: "test-string-value",
+		IntVal:    123,
+		FloatPtr:  &floatVal,
+	})
+	assert.NoError(t, err)
+	cloudEvent := makeEvent(key, topic, content)
+	input, err := convert.CloudEventToInput(context.TODO(), cloudEvent)
+	assert.NoError(t, err)
+	expectedInput := event.NewMessage(key, topic, string(content))
+	assert.Equal(t, expectedInput, input)
+}
+
+// A plain happy case of convert.ToKNativeEventHandler.
+func TestToKNativeEventHandler(t *testing.T) {
+	emptyPipeline := pipeline.New()
+	handleKNativeEvent := convert.ToKNativeEventHandler(
+		convert.CloudEventToInput,
+		emptyPipeline,
+		convert.OutputToCloudResult)
+	responseEvent, result := handleKNativeEvent(context.TODO(), *makeEvent(key, topic, nil))
+	assert.Nil(t, responseEvent)
+	assert.Equal(t, v2.NewHTTPResult(http.StatusOK, "OK"), result)
+}
+
+func makeEvent(key interface{}, topic string, content []byte) *cloudEvents.Event {
+	return &cloudEvents.Event{
+		Context: cloudEvents.EventContextV03{
+			Source:          cloudEventTypes.URIRef{URL: url.URL{Fragment: topic}},
+			DataContentType: cloudEvents.StringOfApplicationJSON(),
+			Extensions: map[string]interface{}{
+				"key": key,
+			},
+		}.AsV03(),
+		DataEncoded: content,
+	}
+}
