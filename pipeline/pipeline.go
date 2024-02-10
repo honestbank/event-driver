698137570c890b2943f66b80b4cb43683eaa6a73
Kelu Diao <1632092+lukecold@users.noreply.github.com>
2024-02-11 00:09:48 +0700

    feat: Make pipeline respect the context timeouts (#32)
    This PR makes the `pipeline.Pipeline` respect `context.Deadline`. 
    
    One could use this feature by passing `context.WithTimeout(parentContext, timeoutDuration)` to `pipeline#Process`, then `pipeline#Process` would return a timeout error immediately when deadline is reached. 
    
    **Please note that the handler may still keep running until it's terminated by itself or when the main goroutine (usually the service) is terminated. One is responsible to make their customized handlers handle timeouts gracefully, to prevent resource leak.**
    
    Also updated the `pipeline.Run` to `pipeline.Process`, since `#Run` gives users an incorrect idea that this may start serving live traffic, rather than just processes one input message.

--- pipeline/pipeline.go
+++ pipeline/pipeline.go
@@ -2,14 +2,23 @@ package pipeline
 
 import (
 	"context"
+	"errors"
+	"log"
 
 	"github.com/lukecold/event-driver/event"
 	"github.com/lukecold/event-driver/handlers"
 )
 
 type Pipeline interface {
+	// WithNextHandler append a handler to the end of the pipeline.
 	WithNextHandler(handler handlers.Handler) Pipeline
-	Run(ctx context.Context, in *event.Message) error
+	// Process executes the handlers in the pipeline in order.
+	// If using customized handlers, please make sure next#Call is executed if it's not the last handler.
+	// Process respects context.Deadline, and would return a timeout error immediately when deadline is reached.
+	// Please note that the handler may still keep running until it's terminated by itself or
+	// when the main goroutine (usually the service) is terminated.
+	// One is responsible to make their customized handlers handle timeouts gracefully, to prevent resource leak.
+	Process(ctx context.Context, in *event.Message) error
 }
 
 type pipeline struct {
@@ -34,15 +43,33 @@ func (n next) Call(ctx context.Context, in *event.Message) error {
 	return n(ctx, in)
 }
 
-func (p *pipeline) Run(pipelineContext context.Context, pipelineInput *event.Message) error {
-	processor := next(func(ctx context.Context, in *event.Message) (_ error) {
-		return
+func (p *pipeline) Process(ctx context.Context, in *event.Message) error {
+	process := next(func(_ context.Context, _ *event.Message) error {
+		return nil
 	})
 	for i := len(p.handlers) - 1; i >= 0; i-- {
-		processor = func(ctx context.Context, in *event.Message) error {
-			return p.handlers[i].Process(ctx, in, processor)
+		index := i
+		processNext := process
+		process = func(handlerCtx context.Context, message *event.Message) error {
+			errorChan := make(chan error)
+			go func(index int, errorChan chan error) {
+				errorChan <- p.handlers[index].Process(handlerCtx, message, processNext)
+			}(index, errorChan)
+
+			select {
+			case gotError := <-errorChan:
+				if gotError != nil {
+					log.Printf("[ERROR] handler at index %d failed with error %v", index, gotError)
+				}
+
+				return gotError
+			case <-handlerCtx.Done():
+				log.Printf("[ERROR] handler at index %d timed out", index)
+
+				return errors.New("pipeline timed out")
+			}
 		}
 	}
 
-	return processor(pipelineContext, pipelineInput)
+	return process(ctx, in)
 }
