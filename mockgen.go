fbcbe20f8dfadde30c33af20a7f823c53f7028ed
Kelu <diaokelu@gmail.com>
2024-04-03 16:02:11 +0700

    feat: Support customizing key extractor in cache
    There's a usecase in Honest Technologies that services do idempotent check on
    keys inside the JSON-typed content, rather than the message key.

--- mockgen.go
+++ mockgen.go
@@ -1,7 +1,7 @@
 //go:build mockgen
 
 //go:generate go run go.uber.org/mock/mockgen -destination=./mocks/mock_handlers.go -package=mocks github.com/lukecold/event-driver/handlers CallNext
-//go:generate go run go.uber.org/mock/mockgen -destination=./mocks/mock_cache.go -package=mocks github.com/lukecold/event-driver/handlers/cache ConflictResolver
+//go:generate go run go.uber.org/mock/mockgen -destination=./mocks/mock_cache.go -package=mocks github.com/lukecold/event-driver/handlers/cache ConflictResolver,KeyExtractor
 //go:generate go run go.uber.org/mock/mockgen -destination=./mocks/mock_event_storage.go -package=mocks github.com/lukecold/event-driver/storage EventStore
 
 package main
